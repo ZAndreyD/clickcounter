00c0588d64d5b3eb31b240b9c3fa83a1bba0f8b6
Зуев Андрей <andrey.dmitrievich96@gmail.com>
2022-07-26 19:48:49 +0300

    Релиз первого проекта-задания Я.практикум iOS

--- clickcounter/ViewController.swift
+++ clickcounter/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelCounterValue: UILabel!
+    @IBOutlet weak var buttonCalculation: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var count: Int = 0
+    
+    @IBAction func updateCounterValue(_ sender: Any) {
+        count += 1
+        labelCounterValue.text = "Значение счётчика: \(count)"
+    }
+    
 }
 
